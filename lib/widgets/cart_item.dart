f533b99c4a56e24b56aef9f14087c2e946981133
Tayyab <tayyab.riu@gmail.com>
2021-09-06 18:52:20 +0500

    making cart item dismissible

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String cartId;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
-  CartItem(this.cartId, this.price, this.quantity, this.title);
+  CartItem(this.cartId, this.productId, this.price, this.quantity, this.title);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Colors.purple,
-            child: Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: FittedBox(child: Text('\$ $price')),
+    return Dismissible(
+      key: ValueKey(cartId),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          size: 24,
+          color: Colors.white,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.purple,
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: FittedBox(child: Text('\$ $price')),
+              ),
             ),
+            title: Text(title),
+            subtitle: Text('Total: \$ ${(price * quantity)}'),
+            trailing: Text('$quantity *'),
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$ ${(price * quantity)}'),
-          trailing: Text('$quantity *'),
         ),
       ),
     );

c133d9530f036c5bb881640794436d28f3358b75
Tayyab <tayyab.riu@gmail.com>
2021-09-13 12:41:34 +0500

    show/hide snackbar

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -29,26 +29,32 @@ class ProductItem extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
           leading: Consumer<Product>(
-              builder: (context, product, child) =>
-                  IconButton(
-                    icon: Icon(
-                        product.isFavorite ? Icons.favorite : Icons
-                            .favorite_border),
-                    color: Theme
-                        .of(context)
-                        .accentColor,
-                    onPressed: () {
-                      product.toggleFavorite();
-                    },
-                  ),
+            builder: (context, product, child) => IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).accentColor,
+              onPressed: () {
+                product.toggleFavorite();
+              },
+            ),
           ),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
-            color: Theme
-                .of(context)
-                .accentColor,
+            color: Theme.of(context).accentColor,
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              //for hiding current snack bar
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              //for showing snack bar
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Item added to card',
+                    textAlign: TextAlign.center,
+                  ),
+                  duration: Duration(seconds: 2),
+                ),
+              );
             },
           ),
         ),
